61a2172fae26f6845393c583f5d55da2ec7db2c0
Alexander Mentyu <notuxius@gmail.com>
2020-11-26 16:51:13 +0200

    refactoring, remove not needed packages

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,80 +1,15 @@
 /// <reference types="cypress" />
 
-const { MailServerControllerApiFactory } = require("mailslurp-client");
-
-describe("Work.ua register/login test", () => {
-  const email = Cypress.env("email");
-  const password = Cypress.env("password");
-  const firstName = Cypress.env("firstName");
-  const lastName = Cypress.env("lastName");
-
-  const registerUrl = Cypress.env("registerUrl");
-  const registerFirstNameField = Cypress.env("registerFirstNameField");
-  const registerLastNameField = Cypress.env("registerLastNameField");
-  const registerSubmitButton = Cypress.env("registerSubmitButton");
-
-  const loginUrl = Cypress.env("loginUrl");
-  const loginUserNameField = Cypress.env("loginUserNameField");
-  const loginPasswordField = Cypress.env("loginPasswordField");
-  const loginSubmitButton = Cypress.env("loginSubmitButton");
-
+describe("registration/login test", () => {
   const loggedInUrlPart = Cypress.env("loggedInUrlPart");
 
-  // let inboxId;
-  // let emailAddress;
-
-  it("can generate a new email address and register", () => {
-    // Cypress.on("uncaught:exception", (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    //   return false;
-    // });
-
-    // cy.createInbox().then((inbox) => {
-    // verify a new inbox was created
-    //   assert.isDefined(inbox);
-
-    // save the inboxId for later checking the emails
-    //   inboxId = inbox.id;
-    //   emailAddress = inbox.emailAddress;
-
-    // register with inbox email address and the password
-    cy.visit(registerUrl);
-
-    cy.get(registerFirstNameField)
-      .type(firstName)
-      .should("have.value", firstName);
-    cy.get(registerLastNameField).type(lastName).should("have.value", lastName);
-    cy.get(loginUserNameField).type(email).should("have.value", email);
-    cy.get(loginPasswordField).type(password).should("have.value", password);
-    cy.get(registerSubmitButton).click();
-
-    cy.url().should("include", loggedInUrlPart);
-    // });
-  });
-
-  it("can verify sucessful login", () => {
-    cy.visit(loginUrl);
-    cy.get(loginUserNameField).type(email).should("have.value", email);
-    cy.get(loginPasswordField).type(password).should("have.value", password);
-    cy.get(loginSubmitButton).click();
-
+  it("can verify registration", () => {
+    cy.register();
     cy.url().should("include", loggedInUrlPart);
   });
 
-  // let code;
-
-  // it("can receive register email and extract register link", () => {
-  //   cy.waitForLatestEmail(inboxId).then((email) => {
-  //     assert.isDefined(email);
-
-  //     // console.log(email.body[1]);
-  //     assert.strictEqual(/123/.test(email.body), true);
-
-  //     code = /([0-9]{6})$/.exec(email.body[1]);
-
-  //     //visit email link
-  //
-  //   });
-  // });
+  it("can verify login", () => {
+    cy.login();
+    cy.url().should("include", loggedInUrlPart);
+  });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,37 +1,33 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
+const email = Cypress.env("email");
+const password = Cypress.env("password");
+const firstName = Cypress.env("firstName");
+const lastName = Cypress.env("lastName");
 
-const { MailSlurp } = require("mailslurp-client");
-const mailslurp = new MailSlurp({ apiKey: Cypress.env("API_KEY") });
+const registerUrl = Cypress.env("registerUrl");
+const registerFirstNameField = Cypress.env("registerFirstNameField");
+const registerLastNameField = Cypress.env("registerLastNameField");
+const registerSubmitButton = Cypress.env("registerSubmitButton");
 
-Cypress.Commands.add("createInbox", () => {
-  // instantiate MailSlurp
-  // return { id, emailAddress } or a new randomly generated inbox
-  return mailslurp.createInbox();
+const loginUrl = Cypress.env("loginUrl");
+const loginUserNameField = Cypress.env("loginUserNameField");
+const loginPasswordField = Cypress.env("loginPasswordField");
+const loginSubmitButton = Cypress.env("loginSubmitButton");
+
+Cypress.Commands.add("register", () => {
+  cy.visit(registerUrl);
+
+  cy.get(registerFirstNameField)
+    .type(firstName)
+    .should("have.value", firstName);
+  cy.get(registerLastNameField).type(lastName).should("have.value", lastName);
+  cy.get(loginUserNameField).type(email).should("have.value", email);
+  cy.get(loginPasswordField).type(password).should("have.value", password);
+  cy.get(registerSubmitButton).click();
 });
 
-Cypress.Commands.add("waitForLatestEmail", (inboxId) => {
-  return mailslurp.waitForLatestEmail(inboxId);
+Cypress.Commands.add("login", () => {
+  cy.visit(loginUrl);
+  cy.get(loginUserNameField).type(email).should("have.value", email);
+  cy.get(loginPasswordField).type(password).should("have.value", password);
+  cy.get(loginSubmitButton).click();
 });
